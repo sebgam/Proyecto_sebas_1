53200a1c7647b50afeb4d1e2bd15ded12320498a
sebas <sebastiangamboa11@gmail.com>
2017-02-13 16:50:24 -0500

    size

---
+++ src/graficos/creando_marco_centrado.java
@@ -0,0 +1,36 @@
+package graficos;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.*;
+
+public class creando_marco_centrado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+class marco_centrado extends JFrame{
+	
+	public marco_centrado()
+	{
+		
+		Toolkit mipantalla = Toolkit.getDefaultToolkit();
+		
+		Dimension tamano = mipantalla.getScreenSize();
+		
+		int alturapantalla = tamano.height;
+		int anchopantalla = tamano.width;
+		
+		
+		
+	}
+	
+	
+	
+}
