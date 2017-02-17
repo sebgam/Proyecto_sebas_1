0dfc71b378985dd84d47a7bb3edfbaad7d09d9b9
sebas <sebastiangamboa11@gmail.com>
2017-02-16 19:05:42 -0500

    graphics

--- src/graficos/prueba_dibujo.java
+++ src/graficos/prueba_dibujo.java
@@ -1,7 +1,8 @@
 package graficos;
 
 import java.awt.Graphics;
-
+import java.awt.Graphics2D;
+import java.awt.geom.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -44,12 +45,29 @@ class laminaDibujo extends JPanel{
 	public void paintComponent( Graphics g){
 		super.paintComponent( g);
 		
-		g.drawRect(50, 50, 250, 100);
-		g.drawString("hola mundo", 100, 80);
+		Graphics2D g2=(Graphics2D) g;
 	     
+		Rectangle2D rectangulo = new Rectangle2D.Double(100,100,200,150);
 		 
+		g2.draw(rectangulo);
 		
+		Ellipse2D elipse= new Ellipse2D.Double();
 		
+		elipse.setFrame(rectangulo);
+		
+		g2.draw(elipse);
+		
+		g2.draw(new Line2D.Double(100, 100, 300, 250));
+		
+		double CentroenX = rectangulo.getCenterX();
+		double centroenI = rectangulo.getCenterY();
+		double radio = 150;
+		
+		Ellipse2D circulo = new Ellipse2D.Double();
+		
+		circulo.setFrameFromCenter(CentroenX, centroenI, CentroenX+radio, centroenI+radio);
+		
+		g2.draw(circulo);
 		
 		
 	}
